7f8e3ee32a2f3dcb3c113b585becd72af41cbe11
Kazuto Yoshioka <gold11house@gmail.com>
2021-08-25 10:08:00 +0900

    first commit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -12,13 +12,25 @@ class RoomsController < ApplicationController
     end
     
     def show
-        @room = Room.find(params[:id])
-        if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
-            @messages = @room.messages
-            @message = Message.new
-            @entries = @room.entries
+        @user = User.find(params[:id]) 
+        @currentUserEntry=Entry.where(user_id: current_user.id)
+        @userEntry=Entry.where(user_id: @user.id)
+        if @user.id == current_user.id
         else
-            redirect_back(fallback_location: root_path)
+          @currentUserEntry.each do |cu|
+            @userEntry.each do |u|
+              if cu.room_id == u.room_id then
+                @isRoom = true
+                @roomId = cu.room_id
+              end
+            end
+          end
+          if @isRoom
+          else
+            @room = Room.new
+            @entry = Entry.new
+          end
         end
     end
 end
+
